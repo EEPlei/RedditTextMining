672748e49622488529124d20d56257366e48144c
Tianyi Wang <tywang918@gmail.com>
2015-11-27 20:19:50 -0500

    added something

--- reddit_rhipe.R
+++ reddit_rhipe.R
@@ -28,125 +28,53 @@ rhoptions(runner = 'sh ./R.Pkg/library/Rhipe/bin/RhipeMapReduce.sh')
 ### put a file to HDFS so we can use it
 #rhput("/data/Shakespeare/hamlet.txt","/data/")
 
-rhls("/data")
+#rhls("/data")
 
-
-### Word Count Example
-
-
-wc_reduce = expression(
-  pre = {
-    total = 0
-  },
-  reduce = {
-    total = total + sum(unlist(reduce.values))
-  },
-  post = {
-    rhcollect(reduce.key, total)
-  }
-)
-
-wc_map = expression({
-  lapply(
-    seq_along(map.keys), 
-    function(r) 
-    {
-      line = tolower(map.values[[r]])
-      line = gsub("[-—]"," ",line)
-      line = gsub("[^'`’[:alpha:][:space:]]","",line,perl=TRUE)
-      line = gsub("(^\\s+|\\s+$)","",line)
-      line = strsplit(line, "\\s+")[[1]]
-      line = line[line != ""]
-      
-      lapply(line, rhcollect, value=1)
+MapReduce <- function(file){
+  user_reduce = expression(
+    pre = {
+      total = 0
+    },
+    reduce = {
+      total = total + sum(unlist(reduce.values))
+    },
+    post = {
+      rhcollect(reduce.key, total)
     }
   )
-})
-
-
-wc = rhwatch(
-  map      = wc_map,
-  reduce   = wc_reduce,
-  input    = rhfmt("/data/Shakespeare/hamlet.txt", type = "text")
-)
-
-
-get_val = function(x,i) x[[i]]
-
-counts = data.frame(key = sapply(wc,get_val,i=1),value = sapply(wc,get_val,i=2), stringsAsFactors=FALSE)
-
-
-
-## Line counting example
-
-lc_reduce = expression(
-  pre = {
-    total = 0
-  },
-  reduce = {
-    total = total + sum(unlist(reduce.values))
-  },
-  post = {
-    rhcollect(reduce.key, total)
-  }
-)
-
-lc_map = expression({
-  lapply(
-    seq_along(map.keys), 
-    function(r) 
-    {
-      rhcollect("line",1)
-    }
-  )
-})
-
-
-lc = rhwatch(
-  map      = lc_map,
-  reduce   = lc_reduce,
-  input    = rhfmt("/data/Shakespeare/hamlet.txt", type = "text")
-  #input    = rhfmt("/data/RC_2015-01.json", type = "text")
-)
-
-
-## User Comment Example
-
-
-user_reduce = expression(
-  pre = {
-    total = 0
-  },
-  reduce = {
-    total = total + sum(unlist(reduce.values))
-  },
-  post = {
-    rhcollect(reduce.key, total)
-  }
-)
-
-user_map = expression({
-  suppressMessages(library(jsonlite))
   
-  lapply(
-    seq_along(map.keys), 
-    function(r) 
-    {
-      key = fromJSON(map.values[[r]])$author
-      value = 1
-      rhcollect(key,value)
-    }
+  user_map = expression({
+    suppressMessages(library(jsonlite))
+    
+    lapply(
+      seq_along(map.keys), 
+      function(r) 
+      {
+        key = fromJSON(map.values[[r]])$subreddit
+        value = 1
+        rhcollect(key,value)
+      }
+    )
+  })
+ 
+  user = rhwatch(
+    map      = user_map,
+    reduce   = user_reduce,
+    input    = rhfmt(file, type = "text")
   )
-})
+  
+  get_val = function(x,i) x[[i]]
+  
+  counts = data.frame(key = sapply(user,get_val,i=1),value = sapply(user,get_val,i=2), stringsAsFactors=FALSE)
+  
+}
 
+files <- c(
+  "/data/RC_2015-01.json",
+  "/data/RC_2015-02.json",
+  "/data/RC_2015-03.json",
+  "/data/RC_2015-04.json",
+  "/data/RC_2015-05.json"
+  )
 
-user = rhwatch(
-  map      = user_map,
-  reduce   = user_reduce,
-  input    = rhfmt("/data/short_1e3.json", type = "text")
-)
-
-
-get_val = function(x,i) x[[i]]
-
-counts = data.frame(key = sapply(user,get_val,i=1),value = sapply(user,get_val,i=2), stringsAsFactors=FALSE)
+monthly_subreddits <- lapply(files,MapReduce)
